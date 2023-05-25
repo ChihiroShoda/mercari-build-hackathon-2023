cee2bbf744d412d705d8dcc2354788adeea17607
rshimaok <rihs14804@gmail.com>
2023-05-25 14:47:32 +0900

    headerを作成 ❗css要確認

--- frontend/simple-mercari-web/src/components/Header/Header.tsx
+++ frontend/simple-mercari-web/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 
 export const Header: React.FC = () => {
   const [cookies, _, removeCookie] = useCookies(["userID", "token"]);
+  const navigate = useNavigate();
 
   const onLogout = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
@@ -12,13 +14,23 @@ export const Header: React.FC = () => {
 
   return (
     <>
+    <head>
+    {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossOrigin="anonymous"></link> */}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"></link>
+    </head>
       <header>
         <p>
-          <b>Simple Mercari</b>
+          <b>MiniMercari</b>
         </p>
-        <div className="LogoutButtonContainer">
-          <button onClick={onLogout} id="MerButton">
-            Logout
+        <div className="ButtonContainer">
+          <button  onClick={() => navigate("/")}>
+            <i className="bi bi-house-door-fill"></i>
+          </button>
+          <button onClick={() => navigate(`/user/${cookies.userID}`)}>
+            <i className="bi bi-person-fill"></i>
+          </button>
+          <button onClick={onLogout}>
+            <i className="bi bi-box-arrow-right"></i>
           </button>
         </div>
       </header>
