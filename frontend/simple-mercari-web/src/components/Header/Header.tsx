96a8b0eea207a904add49be62f704745ed2ead7c
rshimaok <rihs14804@gmail.com>
2023-05-28 10:39:19 +0900

    [add]お気に入りボタンとmodal設置

--- frontend/simple-mercari-web/src/components/Header/Header.tsx
+++ frontend/simple-mercari-web/src/components/Header/Header.tsx
@@ -21,7 +21,7 @@ export const Header: React.FC = () => {
     {/* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossOrigin="anonymous"></link> */}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"></link>
     </head>
-      <header>
+      <header className="header">
         <p>
           <a href="/">MiniMercari</a>
         </p>
@@ -31,6 +31,9 @@ export const Header: React.FC = () => {
           </button>
           <button onClick={() => navigate(`/user/${cookies.userID}`)}>
             <i className="bi bi-person-fill"></i>
+          </button>
+          <button onClick={() => navigate(`/`)}>
+            <i className="bi bi-heart-fill"></i>
           </button>
           <button onClick={onLogout}>
             <i className="bi bi-box-arrow-right"></i>
--- frontend/simple-mercari-web/src/components/Item/Item.tsx
+++ frontend/simple-mercari-web/src/components/Item/Item.tsx
@@ -35,9 +35,65 @@ export const Item: React.FC<{ item: Item }> = ({ item }) => {
     fetchData();
   }, [item]);
 
+  const onSubmit = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const target =  event.currentTarget.parentElement;
+    const icon = target?.querySelector("i");
+    if(icon?.classList.contains("bi-heart-fill")){
+      icon?.classList.remove("bi-heart-fill");
+      icon?.classList.toggle("bi-heart");
+    }else{
+      icon?.classList.remove("bi-heart");
+      icon?.classList.toggle("bi-heart-fill");
+      const modal = document.getElementById("id01");
+      if (modal) {
+        modal.style.display = "block";
+      }
+    }
+  }; 
+
+  const closeModal = () => {
+    const modal = document.getElementById("id01");
+    if (modal) {
+      modal.style.display = "none";
+    }
+  };
+
   return (
-    <div className="w3-card-4">
+    <div className="w3-card-4 itemList">
     <img src={itemImage} alt="..." onClick={() => navigate(`/item/${item.id}`)}></img>
+    <button onClick={onSubmit} id = "favorite">
+      <i className="bi bi-heart"></i>
+    </button>
+    <div id="id01" className="w3-modal">
+    <div className="w3-modal-content">
+      <header className="w3-container"> 
+        <span onClick={closeModal} 
+        className="w3-button w3-display-topright">&times;</span>
+        <h2>Folders</h2>
+      </header>
+      <div className="w3-container" id="select-folder">
+      <form>
+        <p>
+        <input className="w3-radio" type="radio" name="gender" value="male" checked/>
+        <label>Male</label>
+        </p>
+        <p>
+        <input className="w3-radio" type="radio" name="gender" value="female"/>
+        <label>Female</label>
+        </p>
+        <input
+              type="text"
+              name="name"
+              id="MerTextInput"
+              placeholder="+ New Folder"
+        />
+         <button type="submit" id="MerButton">
+              Register
+        </button>
+      </form>
+      </div>
+    </div>
+  </div>
     <div className="w3-container w3-center">
       <b>{item.name}</b>
       <p>{item.price}</p>
